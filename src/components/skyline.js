e06099253895428fbb24a517dd3b76e262c9b255
Anusha Gani <agani@cs.washington.edu>
2024-03-05 10:58:55 -0800

    trying to display skyline onto page (again x2)

--- src/components/skyline.js
+++ src/components/skyline.js
@@ -1,63 +1,66 @@
 
 // import * as d3 from 'd3';
 // precondition: imagedata is an array of objects where each object contains the url of an image and the corresponding tooltip data.
-var imageData = [
-    { url: "src/images/civic.png", tooltipData: "data for image 1" },
-    { url: "src/images/factory.jpeg", tooltipData: "data for image 2" },
-    { url: "src/images/mall.jpeg", tooltipData: "data for image 3" },
-    { url: "src/images/house.jpeg", tooltipData: "data for image 3" },
-    { url: "src/images/office.jpg", tooltipData: "data for image 3" },
-    { url: "src/images/postoffice.png", tooltipData: "data for image 3" },
-    { url: "src/images/restaurant.jpg", tooltipData: "data for image 3" },
-    { url: "src/images/school.jpeg", tooltipData: "data for image 3" },
-    { url: "src/images/soccergoal.jpeg", tooltipData: "data for image 3" },
-    { url: "src/images/university.png", tooltipData: "data for image 3" },
-    { url: "src/images/worship.jpeg", tooltipData: "data for image 3" }
-];
+export default function (element) {
+    var imageData = [
+        {url: "src/images/civic.png", tooltipData: "data for image 1"},
+        {url: "src/images/factory.jpeg", tooltipData: "data for image 2"},
+        {url: "src/images/mall.jpeg", tooltipData: "data for image 3"},
+        {url: "src/images/house.jpeg", tooltipData: "data for image 3"},
+        {url: "src/images/office.jpg", tooltipData: "data for image 3"},
+        {url: "src/images/postoffice.png", tooltipData: "data for image 3"},
+        {url: "src/images/restaurant.jpg", tooltipData: "data for image 3"},
+        {url: "src/images/school.jpeg", tooltipData: "data for image 3"},
+        {url: "src/images/soccergoal.jpeg", tooltipData: "data for image 3"},
+        {url: "src/images/university.png", tooltipData: "data for image 3"},
+        {url: "src/images/worship.jpeg", tooltipData: "data for image 3"}
+    ];
 
 // select the svg element where images will be appended.
-var svg = d3.select("svg");
+    var svg = d3.select("svg");
 
 // define the width and height of each image.
-var imageWidth = 100;
-var imageHeight = 100;
+    var imageWidth = 100;
+    var imageHeight = 100;
 
 // append a group for each image in the imagedata array.
-var images = svg.selectAll("g")
-    .data(imageData)
-    .enter()
-    .append("g")
-    .attr("transform", function(d, i) {
-        return "translate(" + (i * (imageWidth + 10)) + ",0)";
-    })
-    // event listener for mouseover event to show tooltip.
-    .on("mouseover", function(d) {
-        tooltip.transition()
-            .duration(200)
-            .style("opacity", .9);
-        tooltip.html(d.tooltipData)
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-    })
-    // event listener for mouseout event to hide tooltip.
-    .on("mouseout", function(d) {
-        tooltip.transition()
-            .duration(500)
-            .style("opacity", 0);
-    });
+    var images = svg.selectAll("g")
+        .data(imageData)
+        .enter()
+        .append("g")
+        .attr("transform", function (d, i) {
+            return "translate(" + (i * (imageWidth + 10)) + ",0)";
+        })
+        // event listener for mouseover event to show tooltip.
+        .on("mouseover", function (d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html(d.tooltipData)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        // event listener for mouseout event to hide tooltip.
+        .on("mouseout", function (d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
 // append an image element to each group, setting its attributes.
-images.append("image")
-    .attr("xlink:href", function(d) { return d.url; })
-    .attr("width", imageWidth)
-    .attr("height", imageHeight);
+    images.append("image")
+        .attr("xlink:href", function (d) {
+            return d.url;
+        })
+        .attr("width", imageWidth)
+        .attr("height", imageHeight);
 
 // append a tooltip div to the body.
-var tooltip = d3.select("body")
-    .append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
+    var tooltip = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+}
 //
 //     let data = FileAttachment("Copy of The Violence Project Mass Shooter Database - Version 8 (January 2024)  - Full Database.csv").csv({typed: "auto"})
 // let x;
