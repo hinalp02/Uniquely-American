aae79f934785fbe5c0f53f7577872b5c53c25ada
Anusha Gani <agani@cs.washington.edu>
2024-03-05 22:04:27 -0800

    debugging skyline.js

--- src/components/skyline.js
+++ src/components/skyline.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-
+console.log("before skyline method starts");
 export default function(element) {
     // Load CSV data
     d3.csv("../data/skyline.csv").then(function(csvData) {
@@ -7,22 +7,25 @@ export default function(element) {
 
         // Load image data
         var imageData = [
-            {url: require("../images/civic.svg"), id: 0},
-            {url: require("../images/warehouse.svg"), id: 1},
-            {url: require("../images/mall.svg"), id: 2},
-            {url: require("../images/residence.svg"), id: 3},
-            {url: require("../images/office.svg"), id: 4},
-            {url: require("../images/postoffice.svg"), id: 5},
-            {url: require("../images/restaurant.svg"), id: 6},
-            {url: require("../images/school.svg"), id: 7},
+            {url: require("../images/school.svg"), id: 0},
+            {url: require("../images/university.svg"), id: 1},
+            {url: require("../images/civic.svg"), id: 2},
+            {url: require("../images/church.svg"), id: 3},
+            {url: require("../images/mall.svg"), id: 4},
+            {url: require("../images/restaurant.svg"), id: 5},
+            {url: require("../images/office.svg"), id: 6},
+            {url: require("../images/residence.svg"), id: 7},
             {url: require("../images/tent.svg"), id: 8},
-            {url: require("../images/university.svg"), id: 9},
-            {url: require("../images/church.svg"), id: 10}
+            {url: require("../images/warehouse.svg"), id: 9},
+            {url: require("../images/postoffice.svg"), id: 10},
         ];
+    console.log("images and id defined")
 
         // Merge CSV data with image data based on a common identifier
         imageData.forEach(function(image) {
             var matchingRow = csvData.find(function(row) {
+                // console.log(image.id);
+                // console.log("checking id and code match");
                 return row['Location Code'] == image.id;
             });
             if (matchingRow) {
@@ -34,18 +37,23 @@ export default function(element) {
                     Total Number Injured: ${matchingRow['Total Number Injured']}<br>
                     Total Number Killed: ${matchingRow['Total Number Killed']}
                 `;
+                console.log(matchingRow);
             } else {
                 // Handle missing data if necessary
                 image.tooltipData = 'No data available';
             }
+            console.log(image.tooltipData);
         });
 
         // Select the SVG element where images will be appended
         var svg = d3.select(element).append('svg');
+        console.log("image selected to svg");
+
 
         // Define the width and height of each image
         var imageWidth = 50;
         var imageHeight = 50;
+        console.log("image dimensions defined");
 
         // Append images to the SVG
         var images = svg.selectAll("img")
@@ -55,12 +63,14 @@ export default function(element) {
             .attr("src", function(d) { return d.url; })
             .attr("width", imageWidth)
             .attr("height", imageHeight);
+        console.log("image appended to svg");
 
         // Append a tooltip div to the body
         var tooltip = d3.select("body")
             .append("div")
             .attr("class", "tooltip")
             .style("opacity", 1);
+        console.log("tooltip added");
 
         // Event listener for mouseover event to show tooltip
         images.on("mouseover", function(d) {
@@ -71,12 +81,14 @@ export default function(element) {
                 .style("left", (d.clientX) + "px")
                 .style("top", (d.clientY - 28) + "px");
         })
+        console.log("tooltip for mouseover");
             // Event listener for mouseout event to hide tooltip
-            .on("mouseout", function(d) {
+        images.on("mouseout", function(d) {
                 tooltip.transition()
                     .duration(500)
                     .style("opacity", 0);
             });
+        console.log("tooltip for mouseout");
     });
 }
 
@@ -97,7 +109,7 @@ export default function(element) {
 //         {url: require("../images/university.svg"), tooltipData: "data for image 10"},
 //         {url: require("../images/church.svg"), tooltipData: "data for image 11"}
 //     ];
-//     console.log("images and data defined")
+
 //
 //     // select the svg element where images will be appended.
 //     //var svg = d3.select(element).append('svg');
