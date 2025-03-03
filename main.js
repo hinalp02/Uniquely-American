8a76390e43898382fca07ab15e4601cb3dd8c494
hinalp02 <108777737+hinalp02@users.noreply.github.com>
2025-03-02 16:17:34 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,27 +2,17 @@ import * as d3 from 'd3';
 import Tooltip from './components/tooltip.js';
 import USAMap from './components/map.js';
 import Sankey from './components/sankey.js';
+import data from './data/ViolenceProjData.json';
 import Skyline from "./components/skyline.js";
+import skylineData from "./data/skyline.json";
 
-// Load data and initialize visualizations
-fetch('./data/ViolenceProjData.json')
-  .then((response) => response.json())
-  .then((data) => loadMap(data))
-  .catch((error) => console.error('Error loading ViolenceProjData:', error));
-
-fetch('./data/skyline.json')
-  .then((response) => response.json())
-  .then((skylineData) => loadSkyline(skylineData))
-  .catch((error) => console.error('Error loading skyline data:', error));
-
-function loadSkyline(skylineData) {
+function loadSkyline() {
   const element = document.getElementById('skyline');
-  const skyline = new Skyline(element);
-  skyline.render(skylineData);
-  console.log("loadSkyline reached");
+  const skyline = new Skyline(element)
+  console.log("loadSkyline reached")
 }
 
-function loadMap(data) {
+function loadMap() {
   const sortedData = data.sort((a, b) => b["Number Killed"] - a["Number Killed"]);
 
   const container = document.getElementById('map');
@@ -46,11 +36,11 @@ function loadMap(data) {
     `);
   });
 
-  map.points.on("mousemove", tooltip.move);
+  map.points.on("mousemove", tooltip.move)
   map.points.on("mouseout", () => {
     map.unHighlightCircles();
     tooltip.hide();
-  });
+  })
 }
 
 function loadSankey() {
@@ -58,76 +48,10 @@ function loadSankey() {
   const sankey = new Sankey(element)
 }
 
-// Load all visualizations
-loadSankey();
 
-// import * as d3 from 'd3';
-// import Tooltip from './components/tooltip.js';
-// import USAMap from './components/map.js';
-// import Sankey from './components/sankey.js';
-// //import data from './data/ViolenceProjData.json';
-// fetch('./data/ViolenceProjData.json')
-//   .then((response) => response.json())
-//   .then((data) => loadMap(data))
-//   .catch((error) => console.error('Error loading ViolenceProjData:', error));
-// import Skyline from "./components/skyline.js";
-// //import skylineData from "./data/skyline.json";
-// fetch('./data/skyline.json')
-//   .then((response) => response.json())
-//   .then((skylineData) => loadSkyline(skylineData))
-//   .catch((error) => console.error('Error loading skyline data:', error));
-
-
-
-// function loadSkyline(skylineData) {
-//   const element = document.getElementById('skyline');
-//   const skyline = new Skyline(element);
-//   skyline.render(skylineData);
-//   console.log("loadSkyline reached");
-// }
-
-
-
-// function loadMap(data) {
-//   const sortedData = data.sort((a, b) => b["Number Killed"] - a["Number Killed"]);
-
-//   const container = document.getElementById('map');
-
-//   const map = new USAMap(container);
-//   const tooltip = new Tooltip(container);
-
-//   map.update(sortedData, "Number Killed");
-
-//   map.points.on("mouseover", function (evt, d) {
-//     d3.select(this).classed('highlighted', true);
-//     tooltip.show(`
-//       City: ${d.City}<br>
-//       State: ${d.State}<br>
-//       Date: ${d["Full Date"]}<br>
-//       Number Killed: ${d["Number Killed"]}<br>
-//       Number of Injured: ${d["Number Injured"]}<br>
-//       Shooter Knew Victims: ${d["Insider or Outsider"] === 0 ? 'No' : 'Yes'}<br>
-//       Current or Former Employee: ${d["Workplace Shooting"] === 0 ? 'No' : 'Yes'}<br>
-//       Location of Shooting: ${d["Location Specified"]}<br>
-//     `);
-//   });
-
-//   map.points.on("mousemove", tooltip.move);
-//   map.points.on("mouseout", () => {
-//     map.unHighlightCircles();
-//     tooltip.hide();
-//   });
-// }
-
-
-// function loadSankey() {
-//   const element = document.getElementById('sankey')
-//   const sankey = new Sankey(element)
-// }
-
-
-
-// loadSankey();
+loadMap()
+loadSankey()
+loadSkyline()
 
 
 
